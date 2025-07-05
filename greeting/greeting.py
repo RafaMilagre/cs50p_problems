6a20db485d813ed91b7ebc584d2b12778ef08a95
Rafael Milagre <rafaelmilagrearquiteto@gmail.com>
2025-07-04 22:30:48 -0300

    commit greeting folder

---
+++ greeting/greeting.py
@@ -0,0 +1,13 @@
+# def greet(input)
+#    if 'hello' in input: 
+# É O MESMO QUE DIZER AS LINHAS DE CIMA:
+def greet(arroz):
+    if 'hello' in arroz:
+        return 'Hello, there!'
+    else:
+        "I'm not sure what your mean."
+
+
+greeting = greet('hello, computer')
+print(greeting + ' Rafael')
+
