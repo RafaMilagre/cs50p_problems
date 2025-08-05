8facf977e5d667afec2d868a52f0d3044d98ba97
Rafael Milagre <rafaelmilagrearquiteto@gmail.com>
2025-08-04 21:08:52 -0300

    commit aula sobre condicionais

---
+++ conditional_class/compareor.py
@@ -0,0 +1,7 @@
+x = int(input('What is the x? '))
+y = int(input('What is the y? '))
+
+if x < y or x > y:
+    print('x it not equal to y!')
+else:
+    print('x is equal to y!')
---
+++ conditional_class/conditional_class.py
@@ -0,0 +1,8 @@
+x = int(input('What is the x? '))
+y = int(input('What is the y? '))
+if x < y:
+    print('x is less than y!')
+elif x > y:
+    print('x is great than y!')
+else:
+    print('x is equal to y!')
