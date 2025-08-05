af0f75c3708817e7ddcc88234836fd66fce21598
Rafael Milagre <rafaelmilagrearquiteto@gmail.com>
2025-08-04 21:25:04 -0300

    commit ex parity

--- conditional_class/parity.py
+++ conditional_class/parity.py
@@ -1,6 +1,13 @@
-x = int(input('What is x? '))
+def main():
+    x = int(input('What is x? '))
+    if is_even(x):
+        print('Even')
+    else: 
+        print('Odd')
 
-if x % 2 == 0:
-    print('Even')
-else: 
-    print('Odd')
+
+def is_even(n):
+    return True if n % 2 == 0 else False
+
+
+main()
